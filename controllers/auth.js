cd869b3927f4fff26f368fca1de0c0e8d6e2ef1b
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-12-07 16:10:01 +0300

    auth finished (need to add validator)

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 //routes
+//get and post for sign up
 router.get('/sign-up', (req, res) => {
   res.render('auth/sign-up.ejs')
 })
@@ -28,3 +29,38 @@ router.post('/sign-up', async (req, res) => {
     console.log(error)
   }
 })
+//get and post for sign in
+router.get('/sign-in', (req, res) => {
+  res.render('auth/sign-in.ejs')
+})
+
+router.post('/sign-in', async (req, res) => {
+  const userInDatabase = await User.findOne({
+    username: req.body.username
+  })
+  if (!userInDatabase) {
+    return res.send('Wrong username or password')
+  }
+
+  const validPassword = bcrypt.compareSync(
+    req.bosy.password,
+    userInDatabase.password
+  )
+  if (!validPassword) {
+    return res.send('Wrong username or password')
+  }
+
+  //if the username and password ia valid :
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id
+  }
+  res.redirect('/')
+})
+//for sign-out
+router.get('/sign-out', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+module.exports = router
